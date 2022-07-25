3cda3b43ea5ca74fe8559c57faa15b2bee398325
AI Bao <287297138@qq.com>
2022-07-24 21:58:24 -0400

    oracle lab4 Q4

--- 06-Developing Applications Using Oracle 420-987/Lab_4/Lab4.sql
+++ 06-Developing Applications Using Oracle 420-987/Lab_4/Lab4.sql
@@ -243,30 +243,34 @@ END;
 
 --4) Declare a cursor named course_cursor that self-join a table course 
 --(of Registration script) to display course names and its course pre-requisites.
-DECLARE
-    CURSOR course_cursor IS
-    SELECT course_name, prereq
-    FROM course;
-BEGIN
-    OPEN course_cursor;
-    CLOSE course_cursor;
-END;
-
 select * from course;
 
 DECLARE
     CURSOR course_cursor IS
-    SELECT course_name, prereq
-    FROM course;
-    c_name      course.course_name%TYPE;
-    c_prereq    course.prereq%TYPE;
+        SELECT c1.course_name as course, c2.course_name as prereq_course, 
+                c1.prereq as prereq
+        FROM course c1
+        LEFT OUTER JOIN course c2
+        ON c1.prereq = c2.course_no;
+    course_cursor_row   course_cursor%ROWTYPE;
+    dash varchar2(80);
 BEGIN
+    dash := rpad('-', 60, '-');
     OPEN course_cursor;
+    dbms_output.put_line(rpad('Course name', 30, ' ') 
+                    || '|' || lpad('Pre-requisites', 20, ' '));
+    dbms_output.put_line(dash);
     LOOP
         FETCH course_cursor
-        INTO c_name, c_prereq;
+        INTO course_cursor_row;
         EXIT WHEN course_cursor%NOTFOUND;
-        dbms_output.put_line(c_name || ' ' || c_prereq);
+        IF (course_cursor_row.prereq is NULL) THEN
+            dbms_output.put_line(rpad(course_cursor_row.course,30,' ') 
+                            || '|  No Pre-requisites');
+        ELSE
+            dbms_output.put_line(rpad(course_cursor_row.course,30,' ') 
+                            || '|  ' || course_cursor_row.prereq_course); 
+        END IF;
     END LOOP;
     CLOSE course_cursor;
 END;
